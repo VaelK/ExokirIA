723c043198cde8cc58be1120771d120fb731f3b5
VaelK <yvandenis@hotmail.fr>
2020-08-31 13:10:28 +0200

    N figure can be plotted in a grid. Functions modifies figure and do not replace them.

--- main/data_selection.py
+++ main/data_selection.py
@@ -1,30 +1,112 @@
 from ExplorIA.src.interactive_selection import InteractiveSelection
-import numpy as np
+import numpy as np, glob
 from bokeh.plotting import figure
 from bokeh import palettes
 from bokeh.models import ColumnDataSource
+from mppv_GY_SUNR import ROOT_DIR as root_mppv_gy_sunr
+import pyarrow.parquet as pq, pandas as pd, numpy as np
+from sklearn.semi_supervised import LabelPropagation, LabelSpreading
+from sklearn.preprocessing import StandardScaler
 
-def fun_plot_sel(data):
+
+def fun_plot_sel(data, f):
     source = ColumnDataSource(data=dict(x=data[:, 0], y=data[:, 1], color=np.tile('#53777a', len(data))))
-    TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
-    f = figure(tools=TOOLS, plot_width=750, plot_height=750, background_fill_color="#fafafa",
-               title="Selection des données")
-    f.circle('x', 'y', source=source, size=5, color="color", alpha=0.8)
+    f.circle('x', 'y', source=source, size=3, color="color", alpha=0.5)
     return f, source
 
-def fun_plot_res(data, sel_index):
-    f = figure(plot_width=750, plot_height=750, background_fill_color="#fafafa",
-               title="Résultat")
+
+def fun_plot_res(data, sel_index, figs, time):
+    print(sel_index)
+    for f in figs:
+        f.background_fill_color="#fafafa"
+    figs[0].title.text="Résultat clustering"
+    figs[1].title.text="Selection vs temps"
+    figs[2].title.text="Clustering vs temps"
     sources = []
+
+    #traitement clustering
+    label = np.tile(-1, len(data))
     for k, val in sel_index.items():
-        sources.append(ColumnDataSource(data=dict(x=data[val, 0], y=data[val, 1])))
-        f.circle('x', 'y', source=sources[-1], size=5, color=palettes.all_palettes["Category10"][np.mod(k, 10)], alpha=0.8)
-    return f, sources
+        label[val] = k
+    lab_prop = LabelSpreading(kernel="knn", n_jobs=-1)
+    training_data = StandardScaler().fit_transform(np.hstack([data, np.arange(len(data))[:, np.newaxis]]))
+    training_data[:, 2] = training_data[:, 2]*10
+    training_data = training_data[:, 2][:, np.newaxis] #On ne clusterise qu'avec le temps
+    lab_prop.fit(training_data, label[:, np.newaxis])
+    new_label = lab_prop.predict(training_data)
+
+    # plot des résultats du clustering
+    source = []
+    for ilab, lab in enumerate(np.unique(new_label)):
+        source.append(ColumnDataSource(data=dict(x=data[new_label == lab, 0], y=data[new_label == lab, 1])))
+        if lab == -1:
+            color = "black"
+        else:
+            color = palettes.all_palettes["Category10"][10][np.mod(ilab, 10)]
+        figs[0].circle('x', 'y', source=source[-1], size=3, color=color, alpha=0.5)
+    sources.append(source)
+
+    # plot des résultats de la selection vs time
+    source = []
+    for k, val in sel_index.items():
+        source.append(ColumnDataSource(data=dict(x=time[val], y=data[val, 0])))
+        if k == -1:
+            color = "black"
+        else:
+            color = palettes.all_palettes["Category10"][10][np.mod(k, 10)]
+        figs[1].circle('x', 'y', source=source[-1], size=3, color=color, alpha=1)
+    sources.append(source)
+
+    # plot des résultats du clustering vs time
+    source = []
+    for ilab, lab in enumerate(np.unique(new_label)):
+        source.append(ColumnDataSource(data=dict(x=time[new_label == lab], y=data[new_label == lab, 0])))
+        if lab == -1:
+            color = "black"
+        else:
+            color = palettes.all_palettes["Category10"][10][np.mod(ilab, 10)]
+        figs[2].circle('x', 'y', source=source[-1], size=3, color=color, alpha=1)
+    sources.append(source)
+    return figs, sources
 
 def fun_close(selection):
     print(selection)
 
-if __name__ == '__main__':
-    data = np.random.normal(0, 1, (100,2))
 
-    int_sel = InteractiveSelection(data, fun_plot_sel, fun_plot_res, fun_close)
+if __name__ == '__main__':
+    # Pour tous les onduleurs
+    for ond_file in glob.glob(root_mppv_gy_sunr + "/data/data_inv_GY/*.parquet"):
+        if "AMBATOLAMPY" in ond_file:
+            continue
+        data = pq.read_table(ond_file).to_pandas()
+        # removing zero production
+        data = data.loc[~((data['PV Power (kW)'] <= 10 ** -1) | (data['Output Apparent Power (kVA)'] <= 10 ** -1) | (
+                data['P Output (kW)'] <= 10 ** -1))]
+        data.drop(columns=["chunk_inv"], inplace=True)
+        data.dropna(how="all", axis=0, inplace=True)
+        ind_na = data[['Cos Phi', 'Frequency (Hz)', 'Grid RMS Current Phase 1 (A)',
+                       'Grid RMS Current Phase 2 (A)', 'Grid RMS Current Phase 3 (A)',
+                       'Grid RMS Voltage Phase 1 (V)', 'Grid RMS Voltage Phase 2 (V)',
+                       'Grid RMS Voltage Phase 3 (V)', 'Output Apparent Power (kVA)',
+                       'P Output (kW)', 'PV Current (A)', 'PV Power (kW)', 'PV Voltage (V)',
+                       'Q Output (kVAr)', 'eta_meas']].isna().any(axis=1)
+        data = data.loc[~ind_na]
+
+    index = (data.index >= pd.to_datetime("2019-04-01")) & (data.index <= pd.to_datetime("2019-10-01"))
+    data_sample = data.loc[index].sort_index()
+    p_out = data_sample['Output Apparent Power (kVA)'].values
+    p_in = data_sample['PV Power (kW)'].values
+    time = data_sample.index
+
+    from sklearn.cluster import DBSCAN
+    import matplotlib.pyplot as plt
+    ind_large_pord = (p_out>800) & (p_out/p_in > 0.96)
+    db = DBSCAN(n_jobs=-1)
+    cls = db.fit_predict(np.vstack([p_in[ind_large_pord], p_out[ind_large_pord]/p_in[ind_large_pord], np.arange(len(time[ind_large_pord]))]).T)
+    plt.figure()
+    for c in np.unique(cls):
+        plt.scatter(p_in[ind_large_pord][c == cls], p_out[ind_large_pord][c == cls]/p_in[ind_large_pord][c == cls], s=3)
+    plt.scatter(p_in, p_in/p_out, c='k', alpha=0.3, s=3)
+    plt.show(block=False)
+
+    int_sel = InteractiveSelection(np.vstack([p_in, p_out/p_in]).T, 2, 2, fun_close, fun_plot_sel, lambda data, sel_ind, figs: fun_plot_res(data, sel_ind, figs, time))
--- src/interactive_selection.py
+++ src/interactive_selection.py
@@ -1,20 +1,35 @@
 from bokeh.plotting import figure, output_file, show, Figure, curdoc
 from bokeh.models import Button, CustomJS
-from bokeh.layouts import column, row
-
+from bokeh.layouts import column, row, gridplot, Spacer
 
 class InteractiveSelection:
 
-    def __init__(self, data, fun_plot_sel, fun_plot_res, fun_close):
+    def __init__(self, data, nrows, ncols, fun_close, fun_plot_sel, fun_plot_res):
+        """
+        :param data: np.ndarray
+            data that are ploted
+        :param nrows: int
+            Number of rows in the grid of plots
+        :param ncols: int
+            Number of columns in the grid of plots
+        :param fun_close: function
+            Function that close the app
+        :param args:
+            List of function that are called to generate each plots. Plots are displayed from laft to right and top to
+            bottom in the grid plot
+        """
+        self.nrows=nrows
+        self.ncols=ncols
         self.data = data
         self.fun_plot_sel = fun_plot_sel
-        self.fun_plot_res = fun_plot_res
+        self.fun_plots = fun_plot_res
         self.fun_close = fun_close
         self.ind_selected = {}  # Index des differentes selections de données
         self.current_group = 0
 
-        self.source_left = None
-        self.source_right = None
+        self.source_sel = None
+        self.source_res = []
+        self.figs = [figure()]*(nrows*ncols)
         # Les callback et callbacks
         # Ok
         self.ok_butt = Button(label="Ok", button_type="success")
@@ -29,30 +44,65 @@ class InteractiveSelection:
         # Suivant
         self.next_butt = Button(label="Groupe suivant", button_type="success")
         self.next_butt.on_click(self.callback_next)
+        #Calculer
+        self.calc_butt = Button(label="Calculer", button_type="success")
+        self.calc_butt.on_click(self.comput_other_fig)
 
         # Creating server
         from bokeh.server.server import Server
         server = Server({'/': self.bokeh_app}, num_procs=1)
         server.start()
-        print('Opening Bokeh application on http://localhost:5006/')
+        print('Opening Bokeh application on http://localhost')
         server.io_loop.add_callback(server.show, "/")
         server.io_loop.start()
 
     def bokeh_app(self, doc):
         # Générer les plots
-        fig_left, self.source_left = self.fun_plot_sel(self.data)
-        fig_right, self.source_right = self.fun_plot_res(self.data, self.ind_selected)
-        self.source_left.selected.on_change('indices', self.update_sel)
-        # cheking that output is figure
-        assert isinstance(fig_left, Figure)
-        assert isinstance(fig_right, Figure)
-        # check that lasso_select and box_select are in tools
+        gridplot_child = []
+        for nc in range(self.ncols):
+            gridplot_child_row = []
+            for nr in range(self.nrows):
+                if nr == 0 and nc == 0:
+                    TOOLS = "crosshair,pan,wheel_zoom,box_zoom,reset,box_select,lasso_select"
+                    f = figure(tools=TOOLS, background_fill_color="#fafafa",
+                               title="Selection des données")
+                    self.figs[0], self.source_sel = self.fun_plot_sel(self.data, f)
+                    assert isinstance(self.figs[0], Figure)
+                    self.plot_width = self.figs[0].plot_width
+                    self.plot_height = self.figs[0].plot_height
+                else:
+                    self.figs[nc*self.ncols+nr] = figure(plot_width=self.plot_width, plot_height=self.plot_height)
+                gridplot_child_row.append(self.figs[nc*self.ncols+nr])
+            gridplot_child.append(gridplot_child_row)
+        grid = gridplot(gridplot_child, sizing_mode="stretch_both")
+        self.source_sel.selected.on_change('indices', self.update_sel)
 
-        plt_row = row(fig_left, fig_right)
-        prec_ok_next_row = row(self.prec_butt, self.ok_butt, self.next_butt)
-        butt_col = column(self.del_butt, prec_ok_next_row)
-        layout = column(plt_row, butt_col)
-        doc.add_root(layout)
+        prec_next_row = row(self.prec_butt, self.next_butt, sizing_mode="stretch_width")
+        button_pan = column(self.del_butt, prec_next_row, self.calc_butt, self.ok_butt)
+        self.layout = column(grid, row(Spacer(), button_pan, Spacer(), sizing_mode="stretch_width"),
+                             sizing_mode="stretch_width")
+        doc.add_root(self.layout)
+
+    def comput_other_fig(self):
+        for s in self.source_res:
+            if isinstance(s, list):
+                for sub_s in s:
+                    for k, v in sub_s.data.items():
+                        sub_s.data[k] = []
+            else:
+                for k, v in s.data.items():
+                    s.data[k] = []
+        self.figs[1:], self.source_res = self.fun_plots(self.data, self.ind_selected, self.figs[1:])
+        # for nc in range(self.ncols):
+        #     for nr in range(self.nrows):
+        #         if nr == 0 and nc == 0:
+        #             fig = self.layout.children[0].children[1].children[nc*self.ncols + nr][0]
+        #         else:
+        #             assert isinstance(figs[nc * self.ncols + nr - 1], Figure)
+        #             fig = figs[nc * self.ncols + nr - 1]
+        #         fig.plot_width = self.plot_width
+        #         fig.plot_height = self.plot_height
+        #         self.layout.children[0].children[1].children[nc*self.ncols + nr] = (fig, nc, nr)
 
     def callback_del(self):
         """
@@ -67,7 +117,7 @@ class InteractiveSelection:
         :return:
         """
         self.current_group = self.current_group - 1
-        self.source_left.selected.indices = self.ind_selected[self.current_group]
+        self.source_sel.selected.indices = self.ind_selected[self.current_group]
         if self.current_group == 0:
             self.prec_butt.disabled = True
         else:
@@ -81,7 +131,7 @@ class InteractiveSelection:
         self.current_group = self.current_group + 1
         if self.current_group not in self.ind_selected.keys():
             self.ind_selected[self.current_group] = []
-        self.source_left.selected.indices = self.ind_selected[self.current_group]
+        self.source_sel.selected.indices = self.ind_selected[self.current_group]
         self.prec_butt.disabled = False
 
     def update_sel(self, _, old, new):
